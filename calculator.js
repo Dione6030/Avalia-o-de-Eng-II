57dd8c428c441a31713ee056c63099f4ff0f8e7c
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-04-09 15:57:49 -0300

    📝 Creat export for tests

--- calculator.js
+++ calculator.js
@@ -15,4 +15,4 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-console.log(sum(v1, v2));
+module.exports = { sum, sub, div, mult, square }
