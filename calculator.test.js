94663cf5fdf0fa00473d02d1c1bc05df929c9b42
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-04-09 16:13:39 -0300

    ✅ Add test for sub

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,10 @@
 const sum = require('./calculator').sum;
+const sub = require('./calculator').sub;
 
 test('É esperado a soma de 2 numeros', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+test('É esperado a subtração de 2 numeros', () => {
+  expect(sub(5, 3)).toBe(2);
 });
