a1d7e84329b178081bcb37b31306de87591101b8
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-04-10 15:24:07 -0300

    ✅ Add test for div

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,10 @@
 const sum = require('./calculator').sum;
+const div = require('./calculator').div;
 
 test('É esperado a soma de 2 numeros', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+test('É esperado a divisão de 2 numeros', () => {
+  expect(div(10, 2)).toBe(5);
 });
