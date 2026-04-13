24f9dd85e7290fad68512f1646c3cf0bcb016a1b
italo <istalo1@hotmail.com>
2026-04-12 22:35:01 -0300

    ✨ Implement square function

--- calculator.js
+++ calculator.js
@@ -13,10 +13,11 @@ function div(v1, v2) {}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
-    return v1 * v2;
 }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1);
+}
 
 module.exports = { sum, sub, div, mult, square }
