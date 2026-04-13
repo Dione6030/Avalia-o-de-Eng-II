5b7747de93d358c7b4490a14ead13c73f1f6309f
italo <istalo1@hotmail.com>
2026-04-12 23:40:34 -0300

    🐛 Fix mult function that was broken by a bad commit

--- calculator.test.js
+++ calculator.test.js
@@ -12,6 +12,9 @@ test('É esperado a subtração de 2 numeros', () => {
 test('É esperado a divisão de 2 numeros', () => {
   expect(div(10, 2)).toBe(5);
 });
+test('É esperado a multiplicação de 2 numeros', () => {
+  expect(mult(4, 5)).toBe(20);
+});
 test('É esperado a raiz quadrada de um numero', () => {
   expect(square(9)).toBe(3);
 });
