0dc41f1f5f35d61f1bbe0fb5a1bd16f56343cc6d
Dione Fernandes Pinheiro <dionepinheiro6030@gmail.com>
2026-04-12 23:17:39 -0300

    🐛 fixes corrupted subtraction test
    em algum pull request o teste de subtração somiu, aí copiei e colei o que foi testado no codigo.

--- calculator.test.js
+++ calculator.test.js
@@ -1,13 +1,17 @@
 const sum = require('./calculator').sum;
+const sub = require('./calculator').sub;
 const div = require('./calculator').div;
 const square = require('./calculator').square;
 
 test('É esperado a soma de 2 numeros', () => {
   expect(sum(2, 3)).toBe(5);
 });
-
+test('É esperado a subtração de 2 numeros', () => {
+  expect(sub(5, 3)).toBe(2);
+});
 test('É esperado a divisão de 2 numeros', () => {
   expect(div(10, 2)).toBe(5);
+});
 test('É esperado a raiz quadrada de um numero', () => {
   expect(square(9)).toBe(3);
 });
