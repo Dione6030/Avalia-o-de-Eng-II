5a1b4d203ed999a087ee4cd54947ce96fe1c4386
italo <istalo1@hotmail.com>
2026-04-12 22:23:36 -0300

    ✅ Add test for mult

--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,10 @@
 const sum = require('./calculator').sum;
-const sub = require('./calculator').sub;
+const mult = require('./calculator').mult;
 
 test('É esperado a soma de 2 numeros', () => {
   expect(sum(2, 3)).toBe(5);
 });
 
-test('É esperado a subtração de 2 numeros', () => {
-  expect(sub(5, 3)).toBe(2);
+test('É esperado a multiplicação de 2 numeros', () => {
+  expect(mult(2, 3)).toBe(6);
 });
